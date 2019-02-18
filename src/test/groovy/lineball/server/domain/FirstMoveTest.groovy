de78b8036b1b8bdc1e2e4a7351be003d794e50d9
m-gaw <42442918+m-gaw@users.noreply.github.com>
2019-02-18 18:05:49 +0100

    Update src/test/groovy/lineball/server/domain/FirstMoveTest.groovy
    Co-Authored-By: twereski <tomaszwereski@gmail.com>

--- src/test/groovy/lineball/server/domain/FirstMoveTest.groovy
+++ src/test/groovy/lineball/server/domain/FirstMoveTest.groovy
@@ -3,7 +3,7 @@ package lineball.server.domain
 import spock.lang.Specification
 
 class FirstMoveTest extends Specification {
-    def "First player make move - direction 1"() {
+    def "First player makes move - direction 1"() {
         given: "Players accepts game initiation"
         and: "game starts"
         when: "first player make move - direction 1"
