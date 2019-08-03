3139d5e55d9b986768088f86f60c770ba25f275d
twereski <tomaszwereski@gmail.com>
2019-08-03 13:05:02 +0200

    bean add

--- src/main/java/lineball/server/domain/eventstore/EventStoreConfiguration.java
+++ src/main/java/lineball/server/domain/eventstore/EventStoreConfiguration.java
@@ -1,11 +1,13 @@
 package lineball.server.domain.eventstore;
 
 import lineball.server.persistence.memory.EventStoreInMemoryRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EventStoreConfiguration {
 
+    @Bean
     EventPublisher eventPublisher() {
         return new EventPublisher(new EventStoreInMemoryRepository());
     }
